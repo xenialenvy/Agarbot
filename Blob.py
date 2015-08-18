d7e966a910baf226752d2a3dfb04eeb2ce260c09
xenialenvy <xenialenvy@gmail.com>
2015-08-17 22:51:23 -0400

    Bot now detects and avoids walls, detects connected blobs

--- Blob.py
+++ Blob.py
@@ -3,6 +3,23 @@ from math import pi
 from pyautogui import moveTo,position
 from math import cos, sin, atan, sqrt, hypot
 
+def arc_tan(x, y):
+    if x>0 and y>=0:
+        a=atan(y/x)
+    elif x>0 and y<0:
+        a = 2*pi + atan(y/x)
+    elif x<0 and y<=0:
+        a = pi + atan(y/x)
+    elif x<0 and y>0:
+        a = pi + atan(y/x)
+    elif x==0 and y>0:
+        a = pi/2
+    elif x==0 and y<0:
+        a = 3*pi/2
+    else:
+        a = 0
+    return a
+
 
 class Blob:
     def __init__(self, x=0, y=0, r=0, blob_type='unclassified'):
@@ -27,7 +44,8 @@ class Blob:
     def angle_from_center(self):
         if self.__center is not False:
             x, y = self.coord_from_center()
-            return atan(y/x)
+            a = arc_tan(x, y)
+            return a
 
     def dist_from_center(self):
         if self.__center:
@@ -45,6 +63,17 @@ class Blob:
 
     def type(self):
         return self.__type
+
+    def unit_from_center(self):
+        if self.__center:
+            d = self.dist_from_center()
+            v = self.coord_from_center()
+            return v[0]/d, v[1]/d
+
+    def angle_from_center(self):
+        if self.__center is not False:
+            x, y = self.unit_from_center()
+            return arc_tan(x,y)
 
     def __str__(self):
         return "Point:(" + str(round(self.__x, 2)) + "," + str(round(self.__y, 2)) + ")\nRadius: " + str(round(self.__radius, 2))
@@ -72,15 +101,18 @@ class Mouse:
         else:
             return False
 
-    def set_angle(self,angle):
+    def set_angle(self, angle):
         mag = self.__mag
         vec = (mag * cos(angle), mag * sin(angle))
         vec = (vec[0]+self.__center[0], vec[1] + self.__center[1])
-        moveTo(vec[0],self.__top_bar+vec[1])
+        moveTo(vec[0], self.__top_bar+vec[1])
 
     def set_position(self, x, y):
         mag = self.__mag
         current_mag = hypot(x,y)
-        vec = (x/current_mag, y/current_mag)
+        if current_mag:
+            vec = (x/current_mag, y/current_mag)
+        else:
+            vec = (0,0)
         vec = ((mag * vec[0])+self.__center[0], (mag * vec[1]) + self.__center[1])
-        moveTo(vec[0], vec[1])
+        moveTo(vec[0], vec[1] + self.__top_bar)
